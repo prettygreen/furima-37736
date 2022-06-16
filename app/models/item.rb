16b2adaee7cf3750108ca70ecb9eaa862d7de35e
prettygreen <london@cat.cx>
2022-06-17 07:30:05 +0900

    ActiveStorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+
+
+  #validates :image,        presence: true
+
+  has_one_attached :image
+
+  # **先々の実装内容であるため、現時点ではコメントアウトしておく。**
+  #belongs_to :user
+  #has_one :order
 end
