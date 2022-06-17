d893f39f2543dd4239eb76b33816b4ac3f0e88ab
prettygreen <london@cat.cx>
2022-06-17 11:22:46 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,9 +6,19 @@ class Item < ApplicationRecord
   belongs_to :region
   belongs_to :shipping
 
-  #validates :image,        presence: true
-
   has_one_attached :image
+
+  validates :image,        presence: true
+  validates :name,         presence: true
+  validates :text,         presence: true
+  validates :category_id,  numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :postage_id,   numericality: { other_than: 1, message: "can't be blank" }
+  validates :region_id,    numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_id,  numericality: { other_than: 1, message: "can't be blank" }
+  validates :price,        presence: true
+  validates :price,        numericality: { in: 300..9999999 }
+  validates :price,        format: { with: /\A[0-9]+\z/ }
 
   # **先々の実装内容であるため、現時点ではコメントアウトしておく。**
   #belongs_to :user
