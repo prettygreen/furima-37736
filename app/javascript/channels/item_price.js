7f5f76bb7588e26c1ecbe07bfd70ab1e582169ce
prettygreen <london@cat.cx>
2022-06-19 08:25:37 +0900

    販売手数料・利益の計算

---
+++ app/javascript/channels/item_price.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+    const salesProfit = document.getElementById("profit");
+    //salesProfit.innerHTML = (Math.floor(inputValue * 0.9));
+    salesProfit.innerHTML = Math.floor(inputValue - (inputValue * 0.1));
+  });
+});
