46a32870c7b586c7473607e6853659357ce619b6
prettygreen <london@cat.cx>
2022-06-22 17:20:17 +0900

    削除の条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,9 +35,14 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+      item = Item.find(params[:id])
+      if item.user_id == current_user.id
+        if item.destroy
+          redirect_to root_path
+        else
+          render :show
+        end
+      end
   end
 
   private
