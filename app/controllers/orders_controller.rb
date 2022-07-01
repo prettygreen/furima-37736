8fd006d5679ca4060f5d1fc15d72b8359e4161cf
prettygreen <london@cat.cx>
2022-07-01 09:47:24 +0900

    エラーメッセージの表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,7 +6,8 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order_address = OrderAddress.new(order_address_params)
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       @order_address.save
       redirect_to root_path
@@ -16,7 +17,7 @@ class OrdersController < ApplicationController
   end
 
 private
-  def order_address_params
+  def order_params
     params.require(:order_address).permit(:postcode, :region_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
