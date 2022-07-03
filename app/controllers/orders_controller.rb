f538a0846980bcdbdc40dbe706fe98e8ca8fe319
prettygreen <london@cat.cx>
2022-07-03 17:20:26 +0900

    決済機能を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -18,7 +19,16 @@ class OrdersController < ApplicationController
 
 private
   def order_params
-    params.require(:order_address).permit(:postcode, :region_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:postcode, :region_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_69252eec1923a41e5e040752"  
+    Payjp::Charge.create(
+      amount: @item.price,  
+      card: order_params[:token],    
+      currency: 'jpy'                 
+    )
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postcode, :region_id, :city, :block, :building, :phone_number
+  attr_accessor :user_id, :item_id, :postcode, :region_id, :city, :block, :building, :phone_number, :token
 
   with_options presence: true do
     validates :user_id
